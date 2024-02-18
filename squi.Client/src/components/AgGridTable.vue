8204862f8524d8142b327636333e2b93efc11978
CatalinIuga <cataliniuga34@gmail.com>
2024-02-18 18:42:07 +0200

    Update ag grid theme, switching to TanStack Table since its not as customizable as I would wish

--- squi.Client/src/components/AgGridTable.vue
+++ squi.Client/src/components/AgGridTable.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { getTableData, getTableSchema } from "@/service/dataService";
 import { TableSchema } from "@/types/responses";
-import { GridOptions } from "ag-grid-community";
-import "ag-grid-community/styles//ag-grid.css";
-import "ag-grid-community/styles//ag-theme-quartz.css";
+import { ColDef, GridOptions } from "ag-grid-community";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
 import { AgGridVue } from "ag-grid-vue3";
 import { onMounted, ref } from "vue";
 
@@ -15,32 +15,31 @@ const gridOptions: GridOptions = {
   suppressRowClickSelection: true,
 };
 
-const columnDefs = ref<
-  {
-    field: string;
-    checkboxSelection?: boolean;
-    headerCheckboxSelection?: boolean;
-    headerCheckboxSelectionFilteredOnly?: boolean;
-  }[]
->([]);
+const columnDefs = ref<ColDef[]>([]);
 const rowData = ref<Record<string, any>>([]);
 
 onMounted(async () => {
-  let aux = [] as any;
+  let aux = [] as ColDef[];
   await getTableSchema(table).then((data: TableSchema) => {
     aux = data.columns.map((column) => {
       return {
         field: column.name.toString(),
+        editable: true,
+        suppressMovable: true,
       };
     });
   });
 
-  aux[0] = {
-    field: aux[0].field,
+  aux.unshift({
+    sortable: false,
+    filter: false,
+    width: 50,
+    resizable: false,
     checkboxSelection: true,
+    editable: true,
     headerCheckboxSelection: true,
     headerCheckboxSelectionFilteredOnly: true,
-  };
+  });
 
   columnDefs.value = aux;
 
@@ -60,41 +59,64 @@ onMounted(async () => {
 </template>
 
 <style scoped>
-/*
-    --background: 0 0% 100%;
-    --foreground: 240 10% 3.9%;
-
-    --card: 0 0% 100%;
-    --card-foreground: 240 10% 3.9%;
-
-    --popover: 0 0% 100%;
-    --popover-foreground: 240 10% 3.9%;
-
-    --primary: 240 5.9% 10%;
-    --primary-foreground: 0 0% 98%;
-
-    --secondary: 240 4.8% 95.9%;
-    --secondary-foreground: 240 5.9% 10%;
-
-    --muted: 240 4.8% 95.9%;
-    --muted-foreground: 240 3.8% 46.1%;
-
-    --accent: 240 4.8% 95.9%;
-    --accent-foreground: 240 5.9% 10%;
-
-    --destructive: 0 84.2% 60.2%;
-    --destructive-foreground: 0 0% 98%;
-
-    --border: 240 5.9% 90%;
-    --input: 240 5.9% 90%;
-    --ring: 240 5.9% 10%;
-    --radius: 0.5rem;
-    */
 .ag-theme-quartz {
-  --ag-active-color: hsl(240, 5.9%, 10%);
-  --ag-data-color: hsl(240, 5.9%, 10%);
-  --ag-foreground-color: hsl(240, 5.9%, 10%);
+  --background: 0 0% 100%;
+  --foreground: 240 10% 3.9%;
+
+  --card: 0 0% 100%;
+  --card-foreground: 240 10% 3.9%;
+
+  --popover: 0 0% 100%;
+  --popover-foreground: 240 10% 3.9%;
+
+  --primary: 240 5.9% 10%;
+  --primary-foreground: 0 0% 98%;
+
+  --secondary: 240 4.8% 95.9%;
+  --secondary-foreground: 240 5.9% 10%;
+
+  --muted: 240 4.8% 95.9%;
+  --muted-foreground: 240 3.8% 46.1%;
+
+  --accent: 240 4.8% 95.9%;
+  --accent-foreground: 240 5.9% 10%;
+
+  --destructive: 0 84.2% 60.2%;
+  --destructive-foreground: 0 0% 98%;
+
+  --border: 240 5.9% 90%;
+  --input: 240 5.9% 90%;
+  --ring: 240 5.9% 10%;
+  --radius: 0.5rem;
+
+  --ag-active-color: hsl(var(--accent-foreground));
+  --ag-data-color: hsl(var(--foreground));
+  --ag-foreground-color: hsl(var(--foreground));
+  --ag-background-color: hsl(var(--background));
+  --ag-secondary-foreground-color: hsl(var(--secondary-foreground));
+  --ag-header-foreground-color: #64748b;
+  --ag-tooltip-background-color: hsl(var(--popover));
+  --ag-disabled-foreground-color: hsl(var(--muted-foreground));
+  --ag-subheader-background-color: hsl(var(--secondary));
+  --ag-subheader-toolbar-background-color: hsl(var(--secondary));
+  --ag-control-panel-background-color: hsl(var(--secondary));
+  --ag-side-button-selected-background-color: hsl(var(--secondary));
+  --ag-header-column-resize-handle-display: none;
+  --ag-selected-row-background-color: hsl(var(--secondary));
+  --ag-modal-overlay-background-color: hsl(var(--popover));
+  --ag-row-hover-color: hsl(var(--secondary));
+  --ag-column-hover-color: hsl(var(--secondary));
+  --ag-border-size: 1px;
+  /* --ag-grid-size: 4px; */
+  --ag-row-height: 32px;
+  /* --ag-list-item-height: 20px; */
+  --ag-font-size: 14px;
+
   @apply h-full w-full font-menlo;
+}
+
+input[type="checkbox"] {
+  @apply !rounded-none;
 }
 
 .ag-theme-quartz .ag-header-cell-label {
